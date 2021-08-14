4eba125c6a3e5436d19f045d20ed2e25f2035788
venustuspl <venustus.pl@gmail.com>
2021-08-14 11:21:07 +0200

    feature: added AuthorController

---
+++ src/main/java/pl/venustus/Amazon/bookstore/controller/AuthorController.java
@@ -0,0 +1,34 @@
+package pl.venustus.Amazon.bookstore.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.venustus.Amazon.bookstore.domain.Author;
+import pl.venustus.Amazon.bookstore.service.AuthorService;
+
+
+@Controller
+@CrossOrigin
+@RequestMapping("/authors")
+public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Author> add(@RequestBody Author author) {
+        try {
+            Author savedAuthor = authorService.addAuthor(author);
+            return new ResponseEntity<>(author, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
